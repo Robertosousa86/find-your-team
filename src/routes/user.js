156f998ca9494a6550a7296027eb3ac52ae8346d
Roberto Sousa <robertosousa186@gmail.com>
2022-02-01 00:48:40 -0300

    Creating function update

--- src/routes/user.js
+++ src/routes/user.js
@@ -10,5 +10,6 @@ const userController = new UserController(User);
 router.post('/', (req, res) => userController.create(req, res));
 router.get('/', (req, res) => userController.get(req, res));
 router.get('/:id', (req, res) => userController.getById(req, res));
+router.put('/:id', (req, res) => userController.update(req, res));
 
 module.exports = router;
