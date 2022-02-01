0297be1e3978e9af0de2cf9e4937e3926a547803
Roberto Sousa <robertosousa186@gmail.com>
2022-02-01 01:07:07 -0300

    Creating route for remove

--- src/routes/user.js
+++ src/routes/user.js
@@ -11,5 +11,6 @@ router.post('/', (req, res) => userController.create(req, res));
 router.get('/', (req, res) => userController.get(req, res));
 router.get('/:id', (req, res) => userController.getById(req, res));
 router.put('/:id', (req, res) => userController.update(req, res));
+router.delete('/:id', (req, res) => userController.remove(req, res));
 
 module.exports = router;
