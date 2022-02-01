1619866a90a8b8d3bdf3df1335f87572cb2c04c6
Roberto Sousa <robertosousa186@gmail.com>
2022-01-31 23:12:58 -0300

    Creating first route

---
+++ src/routes/user.js
@@ -0,0 +1,12 @@
+// Rota de recursos users
+const express = require('express');
+const User = require('../models/users');
+const UserController = require('../controllers/userController');
+
+const router = express.Router(User);
+
+const userController = new UserController(User);
+
+router.post('/', (req, res) => userController.create(req, res));
+
+module.exports = router;
