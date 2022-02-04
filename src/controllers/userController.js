11a6063ae70cbe594dc7382cb3b0eeaa82fe4430
Roberto Sousa <robertosousa186@gmail.com>
2022-02-04 02:25:38 -0300

    Refactoring methods

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -10,70 +10,91 @@ class UserController {
   }
 
   async create(req, res) {
-    const user = new this.User(req.body);
-    const hash = await bcrypt.hash(user.password, 10);
-
-    user.password = hash;
-
     try {
+      const user = new this.User(req.body);
+      const hash = await bcrypt.hash(user.password, 10);
+
+      user.password = hash;
+
       await user.save();
+
       res.status(201).send({ message: 'Usuário(a) criado(a) com sucesso!' });
     } catch (err) {
-      res.status(422).send(err.message);
+      return res.send(err.message);
     }
   }
 
   async get(req, res) {
     try {
       const users = await this.User.find({});
-      res.send(users);
+
+      if (!users.length)
+        return res
+          .status(400)
+          .send({ message: 'Não há usuários cadastrados no sistema!' });
+
+      return res.send(users);
     } catch (err) {
-      res.status(400).send(err.message);
+      return res.send(err.message);
     }
   }
 
   async getById(req, res) {
-    const {
-      params: { id },
-    } = req;
-
     try {
-      const user = await this.User.find({
-        _id: id,
-      });
+      const user = await this.User.find({ _id: req.params.id });
+
+      if (!user.length)
+        return res
+          .status(400)
+          .send({ message: 'Não há usuário(a) cadastrado(a) com esse id!' });
+
       res.send(user);
     } catch (err) {
-      res.status(400).send(err.message);
+      return res.send(err.message);
     }
   }
 
   async update(req, res) {
-    const user = new this.User(req.body);
-    const hash = await bcrypt.hash(user.password, 10);
-
     try {
+      const user = await this.User.findOne({ _id: req.params.id }).select(
+        '+password'
+      );
+
+      if (!user)
+        return res
+          .status(400)
+          .send({ message: 'Não há usuário(a) cadastrado(a) com esse id!' });
+
+      const hash = await bcrypt.hash(user.password, 10);
+
       await this.User.updateOne(
         { _id: req.params.id },
         { password: hash },
         req.body
       );
 
-      res
+      return res
         .status(200)
         .send({ message: 'Dados do usuário(a) atualizados com sucesso!' });
     } catch (err) {
-      res.status(422).send(err.message);
+      return res.send(err.message);
     }
   }
 
   async remove(req, res) {
     try {
-      await this.User.deleteOne({
-        _id: req.params.id,
-      });
-      res.status(200).send({ message: 'Usuário(a) deletado(a) com sucesso!' });
+      const user = await this.User.deleteOne({ _id: req.params.id });
+
+      if (!user.length)
+        return res
+          .status(400)
+          .send({ message: 'Não há usuário(a) cadastrado(a) com esse id!' });
+
+      return res
+        .status(200)
+        .send({ message: 'Usuário(a) deletado(a) com sucesso!' });
     } catch (err) {
-      res.status(400).send(err.message);
+      return res.send(err.message);
     }
   }
 }
